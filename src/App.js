e6f489db8c9f992afbf01d10ef8fbdb2070af4ae
halvis82 <hhafnor@gmail.com>
2022-05-28 20:34:33 +0200

    finish design and plan

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import './App.css'
+import Timer from "./components/Timer"
 
 function App() {
   return (
     <div className="App">
+      <h1>Multi timers</h1>
+      <div className='timerContainer'>
+        <Timer />
+        <Timer />
+        <Timer />
+        <Timer />
+      </div>
     </div>
   )
 }
